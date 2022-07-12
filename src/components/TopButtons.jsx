f8123c5f7c404cc1f95e4e50273a3ee94dfeabfc
MathFerreira01 <ferreiramatheus124@gmail.com>
2022-07-12 19:35:37 -0300

    criado a pasta components e o component TopButtons

---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function TopButtons() {
+
+    const cities = [
+        {
+            id:1,
+            title:"london"
+        },
+        {
+            id:2,
+            title:"Japan"
+        },
+        {
+            id:3,
+            title:"Tokyo"
+        },
+        {
+            id:4,
+            title:"USA"
+        },
+        {
+            id:5,
+            title:"Brazil"
+        }
+    ]
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TopButtons
