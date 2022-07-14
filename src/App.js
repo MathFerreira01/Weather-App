e3ad063318bdee802b63d59aab76599b38afc04f
MathFerreira01 <ferreiramatheus124@gmail.com>
2022-07-13 23:13:21 -0300

    criado o component forecast

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import Forecast from "./components/Forecast";
 import Inputs from "./components/Inputs";
+import TemperatureAndDetails from "./components/TemperatureAndDetails";
+import TimeAndLocation from "./components/TimeAndLocation";
 import TopButtons from "./components/TopButtons";
 
 function App() {
@@ -6,6 +9,11 @@ function App() {
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 ">
       <TopButtons/>
       <Inputs/>
+
+      <TimeAndLocation/>
+      <TemperatureAndDetails/>
+      <Forecast title="hourly forecast"/>
+      <Forecast title="daily forecast"/>
     </div>
   );
 }
