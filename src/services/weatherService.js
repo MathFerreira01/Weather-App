a9b96289d62d761f50bc9bdf72edb929fca25977
MathFerreira01 <ferreiramatheus124@gmail.com>
2022-07-15 20:28:19 -0300

    feito a requisição da API

---
+++ src/services/weatherService.js
@@ -0,0 +1,13 @@
+const API_KEY = "3b47817dafe2c294b2e1236e93d27fd6";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+const getWeatherData = (infoType, searchParams) => {
+  const url = new URL(BASE_URL + "/" + infoType);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+
+  return fetch(url)
+    .then((res) => res.json())
+    .then((data) => data);
+};
+
+export default getWeatherData 
