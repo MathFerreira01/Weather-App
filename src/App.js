6dd2b1531f9547699e1596be21d8e359ffaddb99
MathFerreira01 <ferreiramatheus124@gmail.com>
2022-07-18 22:43:12 -0300

    realizada a requisição da API

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
+import { useState, useEffect } from "react";
 import Forecast from "./components/Forecast";
 import Inputs from "./components/Inputs";
 import TemperatureAndDetails from "./components/TemperatureAndDetails";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TopButtons from "./components/TopButtons";
-import getWeatherData from "./services/weatherService";
+import getFormattedWeatherData from "./services/weatherService";
 
 function App() {
+  const [query, setQuery] = useState({ q: "berlin" });
+  const [units, setUnits] = useState("metric");
+  const [wheater, setWeather] = useState(null);
 
-  const fetchWeather = async () => {
-    const data = await getWeatherData("weather", {q: "london"});
-    console.log(data)
-  }
+  useEffect(() => {
+    const fetchWeather = async () => {
+      await getFormattedWeatherData({ ...query, units }).then((data) => {
+        setWeather(data);
+      });
+    };
 
-  fetchWeather()
+    fetchWeather();
+  }, [query, units]);
 
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 ">
-      <TopButtons/>
-      <Inputs/>
+      <TopButtons />
+      <Inputs />
 
-      <TimeAndLocation/>
-      <TemperatureAndDetails/>
-      <Forecast title="hourly forecast"/>
-      <Forecast title="daily forecast"/>
+      {wheater && (
+        <div>
+          <TimeAndLocation wheater={wheater}/>
+          <TemperatureAndDetails wheater={wheater}/>
+
+          <Forecast title="hourly forecast" />
+          <Forecast title="daily forecast" />
+        </div>
+      )}
     </div>
   );
 }
--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
+import React from "react";
+import { formatToLocalTime } from "../services/weatherService";
 
-function TimeAndLocation() {
+function TimeAndLocation({ wheather: dt, timezone, name, country }) {
   return (
     <div>
-      <div className='flex items-center justify-center my-6'>
-        <p className='text-white text-xl font-extralight'>
-            Tuesday, 31 May 2022 | Local time: 19:00 PM
+      <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">
+          {formatToLocalTime(dt, timezone)}
         </p>
       </div>
 
-      <div className='flex items-center justify-center my-3'>
-        <p className='text-white text-3xl font-medium'>
-            Berlin, DE
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-medium">
+          {`${name}, ${country}`}
         </p>
       </div>
     </div>
-  )
+  );
 }
 
-export default TimeAndLocation
+export default TimeAndLocation;
