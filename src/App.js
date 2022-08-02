d9599dd218cfa6d8fa569fa765c48f6636c8a4fd
MathFerreira01 <ferreiramatheus124@gmail.com>
2022-08-01 22:23:56 -0300

    adicionado o react-toastify para as notificações do app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
 import Forecast from "./components/Forecast";
 import Inputs from "./components/Inputs";
+import Loading from "./components/Loading";
 import TemperatureAndDetails from "./components/TemperatureAndDetails";
 import TimeAndLocation from "./components/TimeAndLocation";
 import getFormattedWeatherData from "./services/weatherService";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [query, setQuery] = useState({ q: "london" });
@@ -12,7 +15,13 @@ function App() {
 
   useEffect(() => {
     const fetchWeather = async () => {
+      const message = query.q ? query.q : "current location.";
+      toast.info("Fecthing Weather for " + message);
+
       await getFormattedWeatherData({ ...query, units }).then((data) => {
+        toast.success(
+          `Successfully fetched weather for ${data.name}, ${data.country}.`
+        );
         setWeather(data);
       });
     };
@@ -21,17 +30,18 @@ function App() {
   }, [query, units]);
 
   const formatBackground = () => {
-    if(!weather) return 'from-cyan-700 to-blue-700'
-    const threshold = units === 'metric' ? 20 : 60
-    if(weather.temp <= threshold) return 'from-cyan-700 to-blue-700'
+    if (!weather) return "from-cyan-700 to-blue-700";
+    const threshold = units === "metric" ? 20 : 60;
+    if (weather.temp <= threshold) return "from-cyan-700 to-blue-700";
 
-    return 'from-yellow-700 to-orange-700'
-  }
+    return "from-yellow-700 to-orange-700";
+  };
 
   return (
-    <div className={`mx-auto max-w-screen-md mt-12 py-5 px-32 bg-gradient-to-br h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}>
+    <div
+      className={`mx-auto max-w-screen-md mt-12 py-5 px-32 bg-gradient-to-br h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}
+    >
       <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
-
       {weather && (
         <div>
           <TimeAndLocation weather={weather} />
@@ -41,6 +51,8 @@ function App() {
           <Forecast title="daily forecast" items={weather.daily} />
         </div>
       )}
+
+      <ToastContainer autoClose={3000} theme="colored" newestOnTop={true} />
     </div>
   );
 }
---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div class="flex items-center justify-center ">
+      <div class="w-24 h-24 border-l-2 border-white-900 rounded-full animate-spin"></div>
+    </div>
+  );
+}
+
+export default Loading;
